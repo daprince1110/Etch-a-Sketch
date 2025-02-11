57306a7fd42c81211e9c0515de7c984309aa8fc2
daprince1110 <198153437+daprince1110@users.noreply.github.com>
2025-02-10 22:10:41 -0600

    Been trying to fix this bug for hours

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 let range = document.querySelector('input[type="range"]')
 let canvas = document.querySelector("#canvas")
 
-const canvas_size = [canvas.offsetWidth, canvas.offsetHeight]
+
 const border_size = 2
-const final_size = [canvas_size[0]-border_size, canvas_size[1]-border_size]
+const canvas_size = [72-border_size,80-border_size]
+
 
 range.addEventListener('change', function(){
+    
     let children = document.querySelectorAll("#canvas > div")
     for (i = 0; i < children.length; i++){
         canvas.removeChild(children[i])
     }
-    for (i = 0; i < range.value; i++){
-        for (x = 0; x < range.value; x++){
-            console.log("Generating an element")
-            let element = document.createElement("div")
-            console.log((final_size[0])/(range.value*2)+"px")
-            element.style.width = `${(final_size[0]) / range.value}px`;
-            element.style.height = `${(final_size[1]) / range.value}px`;
-            canvas.appendChild(element)
-        }
+
+    for (i = 0; i < range.value*2;i++){
+        let element = document.createElement("div")
+        element.style.width = `${canvas_size[0]/range.value}vw`
+        element.style.height = `${canvas_size[1]/range.value}vh`
+        element.style.border_size = "2px solid black";
+        canvas.appendChild(element);
     }
     console.log("\n\n\n\n\n\n\n")
 })
