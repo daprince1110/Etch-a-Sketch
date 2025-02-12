f67591e6890087ad5e4833415f7616ddb14d82eb
daprince1110 <198153437+daprince1110@users.noreply.github.com>
2025-02-11 19:36:08 -0600

    Basically done. just need to code the drawing mode buttons and the save image button

--- script.js
+++ script.js
@@ -5,20 +5,61 @@ let canvas = document.querySelector("#canvas")
 const border_size = 2
 const canvas_size = [72-border_size,80-border_size]
 
+generateGrid(2);
 
-range.addEventListener('change', function(){
-    
+function generateRandomColor(node){
+    let val = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+    node.style.backgroundColor = val
+    return val;
+}
+
+function generateGrid(size){
+    let element;
+    let node;
+
+    for (i = 0; i < size; i++){
+        element = document.createElement("div")
+        element.style.backgroundColor = "white"
+        canvas.appendChild(element)
+
+        for (j = 0; j < range.value; j++){
+            node = document.createElement("div")
+            node.style.width = `${canvas_size[0]/size}vw`
+            node.style.height = `${canvas_size[1]/size}vh`
+
+            element.appendChild(node)
+        }
+
+    }
+
+    let grids = document.querySelectorAll('#canvas div div');
+
+    for (let i = 0; i < grids.length; i++){
+        grids[i].addEventListener("mouseover", function(){
+            generateRandomColor(grids[i])
+        })
+    }
+}
+
+function clearCanvas(size){
     let children = document.querySelectorAll("#canvas > div")
     for (i = 0; i < children.length; i++){
         canvas.removeChild(children[i])
     }
 
-    for (i = 0; i < range.value*2;i++){
-        let element = document.createElement("div")
-        element.style.width = `${canvas_size[0]/range.value}vw`
-        element.style.height = `${canvas_size[1]/range.value}vh`
-        element.style.border_size = "2px solid black";
-        canvas.appendChild(element);
-    }
-    console.log("\n\n\n\n\n\n\n")
+    generateGrid(range.value)
+}
+
+range.addEventListener('change', function(){
+    
+    clearCanvas()
+
+    generateGrid(range.value)
+
+    document.querySelector("#size_text").textContent = `${range.value} x ${range.value}`;
+
+})
+
+document.querySelector('#clear').addEventListener('click', function(){
+    clearCanvas()
 })
